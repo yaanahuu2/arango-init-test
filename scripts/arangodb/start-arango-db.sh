4dd0db1f8d1f697a4d0c2cf2a886086dd4a7cec0
Geoff <newsopkodon@gmail.com>
2021-12-02 17:03:03 -0800

    Update init script

--- scripts/arangodb/start-arango-db.sh
+++ scripts/arangodb/start-arango-db.sh
@@ -4,52 +4,61 @@ set -e
 
 if [ $1 == "test_arango" ];
 then
-  echo "Setting up seed data for ArangoDB";
+  echo "Setting up test data for ArangoDB";
   PROJECT_ROOT_PATH="$PWD";
+  echo "PROJECT_ROOT_PATH: $PROJECT_ROOT_PATH"
+  ARANGO_DOCKER_LOCAL_DIR="../arango-volume-share";
   ARANGO_DOCKER_VOLUME_DESTINATION="/home/arango-volume-share";
 
   # Create arango volume share location
-  mkdir ../../../arango-volume-share
+  if [ -d "$ARANGO_DOCKER_LOCAL_DIR" ]; then
+    echo "Removing $ARANGO_DOCKER_LOCAL_DIR"
+    rm -rf "$ARANGO_DOCKER_LOCAL_DIR"
+  fi
 
-  # Copy Arango Test Seed Data to share location
-  cp -R ../arango ../../../arango-volume-share/
-  cd ../../../arango-volume-share/
+  mkdir $ARANGO_DOCKER_LOCAL_DIR
+  echo "Created local Docker share directory $ARANGO_DOCKER_LOCAL_DIR"
 
-  ARANGO_VOLUME_PATH="$PWD";
-  # echo $ARANGO_VOLUME_PATH
+  # Copy Arango Test Data to local share location
+  cp -R ./scripts/arango $ARANGO_DOCKER_LOCAL_DIR
+  echo "Copied scripts to local Docker share directory $ARANGO_DOCKER_LOCAL_DIR"
+  cd $ARANGO_DOCKER_LOCAL_DIR
+
+  ARANGO_LOCAL_VOLUME_PATH="$PWD";
+  echo "ARANGO_LOCAL_VOLUME_PATH: $ARANGO_LOCAL_VOLUME_PATH"
 
   cd $PROJECT_ROOT_PATH
 fi
 
-if [ $ARANGO_VOLUME_PATH != "" ];
+if [ $ARANGO_LOCAL_VOLUME_PATH != "" ];
 then
-  ARANGO_VOLUME_PATH_CMD = "-v $ARANGO_VOLUME_PATH:$ARANGO_DOCKER_VOLUME_DESTINATION";
+  ARANGO_LOCAL_VOLUME_PATH_CMD=" -v $ARANGO_LOCAL_VOLUME_PATH:$ARANGO_DOCKER_VOLUME_DESTINATION";
+  echo "ARANGO_LOCAL_VOLUME_PATH_CMD: $ARANGO_LOCAL_VOLUME_PATH_CMD"
 else
-  ARANGO_VOLUME_PATH_CMD = "";
+  ARANGO_LOCAL_VOLUME_PATH_CMD="";
 fi
 
 # Remember to set the following environment variables and to add their values to your .env in project root.
 # ARANGO_DB_SERVER="dbserver";
+# ARANGO_DB_PORT="8529";
 # ARANGO_DB_ROOT_PASSWORD="rootPASSWORD";
 # ARANGO_DB_USER="devtester";
 # ARANGO_DB_USER_PASSWORD="confidential"
 # ARANGO_DB_NAME="testdb";
 
-echo "echo stop & remove old docker [$ARANGO_DB_SERVER] and starting new fresh instance of [$ARANGO_DB_SERVER]"
-(docker kill $ARANGO_DB_SERVER || :) && \
-  (docker rm $ARANGO_DB_SERVER || :) && \
-  docker run -e ARANGO_ROOT_PASSWORD=$ARANGO_DB_ROOT_PASSWORD \
+echo "stop & remove old docker [$ARANGO_DB_SERVER] and starting new fresh instance of [$ARANGO_DB_SERVER]"
+(sudo -u root docker container stop $ARANGO_DB_SERVER || :) && \
+  (sudo -u root docker container rm $ARANGO_DB_SERVER || :) && \
+  sudo -u root docker run -e ARANGO_ROOT_PASSWORD=$ARANGO_DB_ROOT_PASSWORD \
   -e ARANGO_TEST_USER_PASSWORD=$ARANGO_DB_USER_PASSWORD \
-  --name $ARANGO_DB_SERVER -p $ARANGO_DB_PORT:8529 -d$ARANGO_VOLUME_PATH_CMD \
+  --name $ARANGO_DB_SERVER -p $ARANGO_DB_PORT:8529 -d$ARANGO_LOCAL_VOLUME_PATH_CMD \
   arangodb
 
 # wait for pg to start
 echo "sleep wait for arango-db-server [$ARANGO_DB_SERVER] to start";
-sleep 3;
+sudo -u root docker ps | grep "$ARANGO_DB_SERVER"
+wait
 
-# TODO
-# create the db
-# set environment variables
-# create new db through arangosh
-
-docker exec -it $ARANGO_DB_SERVER $ARANGO_DOCKER_VOLUME_DESTINATION/arango-setup.sh
+echo "Run setup script to load test data"
+STARTUP_SCRIPT="$ARANGO_DOCKER_VOLUME_DESTINATION/arango/arango-setup.sh"
+sudo -u root docker exec -it $ARANGO_DB_SERVER $STARTUP_SCRIPT
